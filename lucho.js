b648e9bb25df79c7573ecad0ca8597e742c06f4c
alumno <alumno@Instituto.local>
2023-09-15 09:26:38 -0300

    archivos

---
+++ lucho.js
@@ -0,0 +1,29 @@
+function salarioAnual (A)
+{
+    let salario = 21600 / 12 
+    return salario
+}
+ console. log(salarioAnual())
+  
+ function salarioMensual (A)
+ {
+     let salariomes = 1500 * 12
+     return salariomes
+ }
+console. log(salarioMensual())
+
+ function notaPromedio (nota1, nota2, nota3)
+ {
+     let promedio = (nota1 + nota2 + nota3) /3
+     return promedio
+ }
+ 
+ nota1 = Number(prompt('ingresa tu primer  nota '))
+ nota2 = Number(prompt('ingrsa tu segunda nota'))
+ nota3 = Number(prompt('ingresa tu tercer nota'))
+
+
+
+
+alert = (notaPromedio (nota1, nota2, nota3))
+ 
